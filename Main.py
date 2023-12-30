c0ba2da2f4408527d672f450f8b80a8e2a9aa379
MisterEggy <155089632+MisterEggy@users.noreply.github.com>
2023-12-30 18:57:16 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,80 @@
-print("Hello")
+#name = "Timothy Ijaseun"
+#first_name = name[0:7]
+#print(first_name)
+#last_name = name[8:]
+#print(last_name)
+#cool_name = name[::3]
+#print(cool_name)
+#reversed_name = name[::-1]
+#print(reversed_name)
+#website = "www.google.com"
+#slice = slice(4, -4)
+#print(website[slice])
+#website2 = "www.monkeyssssssssssssssssssssssssssssssssss.com"
+#print(website2[slice])
+#age = #int(input("How old are you dear fellow?"))
+#if age >=18 :
+    #print("You are an adult")
+#elif age <0 :
+    #print("Invalid Age, Please try again")
+#else: print("You are child tee hee")
+#name = ""
+#while len(name) == 0:
+    #name = input("Enter your name")
+    #print("Give me that shit young man")
+#print("Hello " + name)
+#for i in range(-10,10):
+    #print(i+1)
+#for i in "4 Big guys": 
+    #print (i)
+
+
+
+#rows = int(input("How many rows? : "))
+#columns = int(input("How many columns? : "))
+#symbol = input("What symbol? ")
+
+#for i in range(rows):
+    #for i in range (columns):
+        #print(symbol, end="")
+#    print()
+
+
+
+
+#for i in range(1,10):
+    #if i == 5:
+        #pass
+    #else:
+        #print(i)
+#capitals = {'USA':'Washington DC',
+            #'Poland':'Warsaw',
+            #'Germany':'Berlin'}
+
+#name = input("What is your name?:  ")
+#if(name[0].islower()):
+    #name = name.capitalize()
+#print("So your name is " + name )
+
+#def hello(first_name,last_name):
+    #print("Hello "+ first_name + "  " + last_name)
+
+#hello("Bro","Code")
+
+def multiply(A,B):
+    return A * B
+    
+number1 = (input("What number will be multiplied? : "))
+number2 = (input("What will it be multiplied by? : "))
+
+x = multiply(number1,number2)
+
+print("Answer = " + str(x))
+
+           
+
+
+
+
+
+   
